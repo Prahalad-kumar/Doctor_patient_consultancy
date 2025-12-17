323974bf3789b5e3a70ad6883bf5421709db7274
InDiAnHeLL <prahaladkr1@gmail.com>
2025-12-17 12:53:31 +0530

    configure the templates path

--- doctor_patient_consultancy/settings.py
+++ doctor_patient_consultancy/settings.py
@@ -58,7 +58,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [BASE_DIR / "templates",
-                 BASE_DIR/'index.html'],
+                BASE_DIR/""],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
