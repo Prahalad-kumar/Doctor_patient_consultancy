1a365e0217b8f67903119f8bda2a4e49dfd7a159
InDiAnHeLL <prahaladkr1@gmail.com>
2025-12-17 12:52:20 +0530

    fixing the templates

--- doctor_patient_consultancy/settings.py
+++ doctor_patient_consultancy/settings.py
@@ -57,7 +57,8 @@ ROOT_URLCONF = 'doctor_patient_consultancy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],
+        'DIRS': [BASE_DIR / "templates",
+                 BASE_DIR/'index.html'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
